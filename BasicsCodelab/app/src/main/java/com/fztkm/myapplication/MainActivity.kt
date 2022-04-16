557d28e10aff343684f71383ded15a0a0c9b7687
Takumi Fukuzawa <forthune.0517@gmail.com>
2022-04-16 11:52:35 +0900

    Composeの状態

--- BasicsCodelab/app/src/main/java/com/fztkm/myapplication/MainActivity.kt
+++ BasicsCodelab/app/src/main/java/com/fztkm/myapplication/MainActivity.kt
@@ -3,11 +3,16 @@ package com.fztkm.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,15 +22,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           MyApp()
+            MyApp()
         }
     }
 }
 
 @Composable
-private fun MyApp(names: List<String> = listOf("android", "Kotlin")){
-    Column{
-        for(name in names){
+private fun MyApp(names: List<String> = listOf("android", "Kotlin")) {
+    Column {
+        for (name in names) {
             Greeting(name = name)
         }
     }
@@ -33,10 +38,28 @@ private fun MyApp(names: List<String> = listOf("android", "Kotlin")){
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(4.dp)){
-        Column(modifier = Modifier.padding(24.dp).fillMaxWidth()){
-            Text("Hello,")
-            Text("$name")
+
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(4.dp),
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text("Hello,")
+                Text("$name")
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value },
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
         }
     }
 }
@@ -45,6 +68,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-       MyApp()
+        MyApp()
     }
 }
