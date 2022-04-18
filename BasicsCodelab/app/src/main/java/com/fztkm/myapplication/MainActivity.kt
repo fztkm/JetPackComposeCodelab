fdbff36a5ec7c766bea5093a90dc66e2f2174bfe
Takumi Fukuzawa <forthune.0517@gmail.com>
2022-04-18 16:49:20 +0900

    completed

--- BasicsCodelab/app/src/main/java/com/fztkm/myapplication/MainActivity.kt
+++ BasicsCodelab/app/src/main/java/com/fztkm/myapplication/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.fztkm.myapplication
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.fztkm.myapplication.ui.theme.MyApplicationTheme
@@ -22,52 +28,124 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp()
+            MyApplicationTheme {
+                MyApp()
+            }
         }
     }
 }
 
 @Composable
-private fun MyApp(names: List<String> = listOf("android", "Kotlin")) {
-    Column {
-        for (name in names) {
-            Greeting(name = name)
+private fun MyApp() {
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
+    if (shouldShowOnBoarding) {
+        OnboardingScreen {
+            shouldShowOnBoarding = false
+        }
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = List(20) { "$it" }) {
+    Surface(color = MaterialTheme.colors.background) {
+        LazyColumn {
+            items(items = names) { name ->
+                Greeting(name)
+            }
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+//    val extraPadding by animateDpAsState(
+//        if (expanded) 48.dp else 0.dp,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow
+//        )
+//    )
 
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
-
-    Surface(
-        color = MaterialTheme.colors.primary,
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(4.dp),
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
+        Row(modifier = Modifier.padding(12.dp)) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(12.dp)
+                    .animateContentSize(
+                        animationSpec = spring(
+                            dampingRatio = Spring.DampingRatioMediumBouncy,
+                            stiffness = Spring.StiffnessLow
+                        )
+                    )
             ) {
                 Text("Hello,")
-                Text("$name")
+                Text(
+                    "$name", style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+                if (expanded) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4)
+                    )
+                }
             }
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value },
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess
+                    else Icons.Filled.ExpandMore,
+                    contentDescription =
+                    if (expanded) stringResource(id = R.string.show_less)
+                    else stringResource(id = R.string.show_more)
+                )
             }
         }
     }
 }
 
-@Preview(showBackground = true, name = "Text Preview", widthDp = 320)
+@Preview(
+    showBackground = true,
+    name = "Text Preview",
+    widthDp = 320,
+    heightDp = 320,
+    uiMode = UI_MODE_NIGHT_YES
+)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to the Basics Codelab!")
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(onClick = onContinueClicked) {
+                Text(text = "Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnBoardingPreview() {
+    MyApplicationTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
