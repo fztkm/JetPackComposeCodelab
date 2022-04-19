eff8146473b88ba5cb83e5361d433eefebb6c0c9
Takumi Fukuzawa <forthune.0517@gmail.com>
2022-04-19 22:11:19 +0900

    マテリアルコンポーネント(Scaffold)

--- LayoutsCodelab/app/src/main/java/com/fztkm/layoutscodelab/MainActivity.kt
+++ LayoutsCodelab/app/src/main/java/com/fztkm/layoutscodelab/MainActivity.kt
@@ -4,24 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Shop
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import com.fztkm.layoutscodelab.ui.theme.LayoutsCodelabTheme
-import kotlin.coroutines.coroutineContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +41,79 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PhotographerCard(modifier: Modifier = Modifier){
+fun LayoutsCodelab() {
+    Scaffold(
+        topBar = {
+            TopBarContet()
+        },
+        floatingActionButton = {
+            FloatingActionButtonContent()
+        },
+        bottomBar = {
+            BottomBarContent()
+        }
+    ) { innerPadding ->
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun TopBarContet(modifier: Modifier = Modifier) {
+    TopAppBar(title = {
+        Text(
+            text = "Layouts Codelab",
+            modifier = modifier,
+        )
+    },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Filled.Favorite, contentDescription = null)
+            }
+        }
+    )
+}
+
+@Composable
+fun FloatingActionButtonContent(modifier: Modifier = Modifier) {
+    FloatingActionButton(onClick = { /*TODO*/ }) {
+        Icon(Icons.Filled.Add, contentDescription = null)
+    }
+}
+
+@Composable
+fun BottomBarContent(modifier: Modifier = Modifier) {
+    BottomAppBar {
+        IconButton(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+            Icon(Icons.Filled.Shop, contentDescription = null)
+        }
+        IconButton(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+            Icon(Icons.Filled.Build, contentDescription = null)
+        }
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier) {
+        Text("Hi there!")
+        Text("Thanks for going through the Layouts Codelab!")
+    }
+}
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    LayoutsCodelabTheme {
+        LayoutsCodelab()
+    }
+}
+
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
     Row(
         modifier
             .padding(5.dp)
@@ -53,14 +125,14 @@ fun PhotographerCard(modifier: Modifier = Modifier){
             .padding(4.dp)
             .clip(RoundedCornerShape(4.dp))
             .background(MaterialTheme.colors.surface)
-            .clickable {  }
+            .clickable { }
             .padding(16.dp)
     ) {
         Surface(
             modifier = Modifier.size(50.dp),
             shape = CircleShape,
             color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-        ){
+        ) {
 
         }
         Column {
@@ -74,7 +146,7 @@ fun PhotographerCard(modifier: Modifier = Modifier){
 
 @Preview(showBackground = false)
 @Composable
-fun PhotographerCardPreview(){
+fun PhotographerCardPreview() {
     LayoutsCodelabTheme {
         PhotographerCard()
     }
